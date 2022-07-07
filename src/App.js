b1c08b44ffaee7d3153c674c177843cf299aa273
RDGU <raydanielgu@gmail.com>
2022-07-07 18:51:30 +0700

    adding es to SearchBox className

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ const App = () => {
       />
       <br />
       <SearchBox
-        className='title-search-boxx'
+        className='title-search-boxxes'
         onChangeHandler={onTitleChange}
         placeholder='Set title'
       />
